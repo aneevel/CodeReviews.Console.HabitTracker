95bc83e226105ea8c3db94db3e36153fd606a7de
aneevel <aneevel15@gmail.com>
2025-12-10 15:09:38 -0600

    initial commit

---
+++ HabitLogger/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HabitLogger
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Habit Logger, C# edition!");
+            Console.WriteLine("-----------------------------------\n");
+
+            while (true)
+            {
+                Console.WriteLine("MAIN MENU\n");
+                Console.WriteLine("Please choose an option:\n");
+                Console.WriteLine("0 - Close Application\n1 - View all Habits\n2 - Insert a new Habit\n3 - Update an existing Habit\n4 - Delete an existing Habit");
+                Console.WriteLine("--------------------------------\n");
+
+                string? input = Console.ReadLine();
+                if (!Int32.TryParse(input, out int option) || !(0 <= option && option <= 5))
+                {
+                    Console.WriteLine("Invalid option chosen. Please choose one of the provided options.");
+                    continue;
+                }
+            }
+        }
+    }
+}
