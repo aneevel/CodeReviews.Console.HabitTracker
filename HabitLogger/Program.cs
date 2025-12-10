f25f51bb7b6a22f3e4ea29f34858536bf72de8c5
aneevel <aneevel15@gmail.com>
2025-12-10 16:13:18 -0600

    create and display main menu

--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace HabitLogger
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitLogger
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            string connectionString = @"Data Source=habit-tracker.db";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = 
+                    @"CREATE TABLE IF NOT EXISTS habits (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Habit TEXT,
+                        Date TEXT,
+                        Quantity INTEGER)";
+                tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+
             Console.WriteLine("Welcome to Habit Logger, C# edition!");
             Console.WriteLine("-----------------------------------\n");
 
@@ -20,7 +39,58 @@
                     Console.WriteLine("Invalid option chosen. Please choose one of the provided options.");
                     continue;
                 }
+
+                switch (option)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        HandleViewHabits();
+                        break;
+                    case 2:
+                        HandleInsertHabit();
+                        break;
+                    case 3:
+                        HandleUpdateHabit();
+                        break;
+                    case 4:
+                        HandleDeleteHabit();
+                        break;
+                }
             }
+        }
+
+        static void HandleViewHabits()
+        {
+            while (true)
+            {
+                Console.WriteLine("Displaying all habits");
+                Console.WriteLine("---------------------");
+
+                Console.WriteLine("Press any key to return to main menu...");
+                string? input = Console.ReadLine();
+
+                if (input != null)
+                {
+                    break;
+                }
+            }
+        }
+
+        static void HandleInsertHabit()
+        {
+
+        }
+
+        static void HandleUpdateHabit()
+        {
+
+        }
+
+        static void HandleDeleteHabit()
+        {
+
         }
     }
 }
