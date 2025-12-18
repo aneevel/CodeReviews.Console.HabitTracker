5993b90b6e96456ea8cd3b63928a1e1d60dbcc7b
aneevel <aneevel15@gmail.com>
2025-12-18 16:19:03 -0600

    provide documentation for all classes

--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -4,6 +4,9 @@ using LoggerEngine.Database;
 
 namespace HabitLogger
 {
+    /// <summary>
+    /// HabitLogger application for tracking occurrence and quantity of specific habits.
+    /// </summary>
     class Program
     {
         public static void Main(string[] args)
